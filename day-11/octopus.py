81ff851d58c640ba188ee7cc0583f6d0b1e1a58e
normanthesquid <normanthesquid@gmail.com>
2021-12-11 09:06:10 +0100

    day 11 part 2

--- day-11/octopus.py
+++ day-11/octopus.py
@@ -1,16 +1,18 @@
-
 from typing import List
 
 
 class octopus:
     def __init__(self, energy_level):
-        self.energy_level = energy_level        
-        self.neighbors: List['octopus'] = []
-        
+        self.energy_level = energy_level
+        self.neighbors: List["octopus"] = []
+
         self.flashed_on_step = [False for _ in range(100)]
         self.flash_count = 0
 
     def increase_energy(self, step):
+        if len(self.flashed_on_step) < step + 1:
+            self.flashed_on_step.append(False)
+
         self.energy_level += 1
         if self.energy_level > 9 and self.flashed_on_step[step] == False:
             self.flashed_on_step[step] = True
@@ -21,8 +23,3 @@ class octopus:
     def end_step(self):
         if self.energy_level > 9:
             self.energy_level = 0
-
-    
-        
-
-    
